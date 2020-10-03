11688272f43a86a0e61f8a4fb1c928cc33bc00ab
Digx7 <webberboy01@yahoo.com>
2020-10-02 22:41:31 -0700

    Porting project from workshop over to repo

---
+++ Assets/Projects/2D Side scroller/Inputs/PaperPlayer.cs
@@ -0,0 +1,143 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Projects/2D Side scroller/Inputs/PaperPlayer.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PaperPlayer : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PaperPlayer()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PaperPlayer"",
+    ""maps"": [
+        {
+            ""name"": ""Movement"",
+            ""id"": ""d9fc7bc3-a7ab-4136-9141-15269c13a44e"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""6c5c85f0-e9e3-4e3b-b655-0cd0c7dfb0fe"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""8432029d-c882-4741-ac95-4073651a3729"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""387ca1e6-4d80-4b8b-803d-fe7ee8e07500"",
+                    ""path"": ""<Gamepad>/dpad"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Movement
+        m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
+        m_Movement_Move = m_Movement.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Movement
+    private readonly InputActionMap m_Movement;
+    private IMovementActions m_MovementActionsCallbackInterface;
+    private readonly InputAction m_Movement_Move;
+    public struct MovementActions
+    {
+        private @PaperPlayer m_Wrapper;
+        public MovementActions(@PaperPlayer wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Movement_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Movement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MovementActions set) { return set.Get(); }
+        public void SetCallbacks(IMovementActions instance)
+        {
+            if (m_Wrapper.m_MovementActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_MovementActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_MovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public MovementActions @Movement => new MovementActions(this);
+    public interface IMovementActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Projects/2D Side scroller/Scripts/PlayerMovement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject playerCharacter;
+    public Rigidbody playerRB;
+    public GameObject playerCharacterSprite;
+    public Vector2 movementInput;
+    public int speed;
+    public int gravityForce;
+    [Space]
+    public PaperPlayer PaperPlayer;
+    public Animator paperPlayerAnimator;
+    [Space]
+    private bool doOnce_R = true, doOnce_L = false;
+
+    public void Awake ()
+    {
+      PaperPlayer = new PaperPlayer();
+
+      // set up inputs
+      PaperPlayer.Movement.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void FixedUpdate ()
+    {
+      move(movementInput);
+    }
+
+
+
+    public void move (Vector2 input)
+    {
+      if(input.x >= 0.1 || input.x <= -0.1)
+      {
+        //Vector3 target = new Vector3(playerCharacter.transform.position.x + input.x,0,0);
+        //playerCharacter.transform.position = Vector3.MoveTowards(playerCharacter.transform.position, target, Time.deltaTime * speed);
+        Vector3 move = new Vector3(input.x * speed,0,0);
+        playerRB.velocity = move;
+
+        paperPlayerAnimator.SetBool("isMoving",true);
+
+        //playerCharacterSprite.transform.LookAt(target);
+        if(input.x > 0)
+        {
+          // right
+          if (!doOnce_R)
+          {
+            doOnce_R = true;
+            doOnce_L = false;
+
+            paperPlayerAnimator.SetTrigger("Flip");
+          }
+        }
+        else if(input.x < 0)
+        {
+          // left
+          if (!doOnce_L)
+          {
+            doOnce_L = true;
+            doOnce_R = false;
+
+            paperPlayerAnimator.SetTrigger("Flip");
+          }
+        }
+      }
+      else
+      {
+        paperPlayerAnimator.SetBool("isMoving",false);
+        playerRB.velocity = new Vector3(0,0,0);
+      }
+    }
+
+    private void OnEnable()
+    {
+      PaperPlayer.Enable();
+    }
+
+    private void OnDisable()
+    {
+      PaperPlayer.Disable();
+    }
+
+}
